b1797f25ca1b1e56738caf11b60820e8732874bf
karinaornelas24 <75212858+karinaornelas24@users.noreply.github.com>
2021-04-09 20:50:58 +0200

    Added Github Url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by Ana Karina Ornelas, open-sourced on{" "}
+        <a
+          href="<https://github.com/karinaornelas24/react-weather-app"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
