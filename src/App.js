f94a53a53c9d862b8bfc9bbe630cd67b9912e9d3
karinaornelas24 <75212858+karinaornelas24@users.noreply.github.com>
2021-04-11 11:30:06 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Coded by Ana Karina Ornelas, open-sourced on{" "}
-        <a
-          href="<https://github.com/karinaornelas24/react-weather-app"
-          target="_blank"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          Coded by Ana Karina Ornelas, open-sourced on{" "}
+          <a
+            href="<https://github.com/karinaornelas24/react-weather-app"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
