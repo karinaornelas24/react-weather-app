2235540236a8e80819524ccadf51a0ef0c467df2
karinaornelas24 <75212858+karinaornelas24@users.noreply.github.com>
2021-04-22 13:36:45 +0200

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
           <a
             href="<https://github.com/karinaornelas24/react-weather-app"
             target="_blank"
+            rel="noopener noreferrer"
           >
             Github
           </a>
